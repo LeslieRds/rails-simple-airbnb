d3a464f88bc4ebad769bb81906d45c9b755553a8
Leslie Rados <leslie.rados@gmail.com>
2023-06-14 17:04:03 +0200

    bnbria elpmis sliar step 2 flats cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+4.times do
+  Flat.create!(
+    name: Faker::Fantasy::Tolkien.character,
+    address: Faker::Address.street_address,
+    description: Faker::ChuckNorris.fact,
+    price_per_night: Faker::Number.digit,
+    number_of_guests: Faker::Number.digit
+  )
+end
